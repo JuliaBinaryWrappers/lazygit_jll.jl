0986081670467b33819286207549af702126ea76
jlbuild <juliabuildbot@gmail.com>
2026-05-26 16:08:59 +0000

    lazygit_jll build 0.61.1+0

--- src/lazygit_jll.jl
+++ src/lazygit_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("lazygit")
-JLLWrappers.@generate_main_file("lazygit", UUID("01a6f0b2-7079-58b6-a815-2223d49c44bc"))
+JLLWrappers.@generate_main_file("lazygit", Base.UUID("01a6f0b2-7079-58b6-a815-2223d49c44bc"))
 end  # module lazygit_jll
